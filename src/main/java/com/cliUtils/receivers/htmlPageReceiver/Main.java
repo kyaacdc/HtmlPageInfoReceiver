40bbf23918b5f9cb084f71bc84abbb645d1dd603
Yuriy <kya@bk.ru>
2017-03-05 17:38:14 +0200

    Add intro() method in Main class, that should for ask and check page Url from user.

---
+++ src/main/java/com/cliUtils/receivers/htmlPageReceiver/Main.java
@@ -0,0 +1,29 @@
+package com.cliUtils.receivers.htmlPageReceiver;
+
+import java.util.Scanner;
+
+public class Main{
+
+    private static String url = "";
+
+    public static void intro(String[] args){
+
+        Scanner sc;
+
+        System.out.println("Welcome to Html Page Words Receiver!");
+
+        if(args.length != 0)
+            url = args[0];
+        else {
+            System.out.println("Please, input Url for receive info");
+            sc = new Scanner(System.in);
+            url = sc.nextLine();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        intro(args);
+
+    }
+}
